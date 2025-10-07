9015381cfd445ef2ad8437f4b99a66e620d9f99b
divya-kaushikkk <divyakaushik0117@example.com>
2025-10-07 17:46:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from "express";
 import AuthRouter from "./routes/user.js"
 import dotenv from "dotenv";
-import { Users } from "./model/user.js";
-import { success } from "zod";
 import ProductRouter from "./routes/product.js";
 
 const app = express();
@@ -11,17 +9,6 @@ dotenv.config();
 
 app.use("/api/auth", AuthRouter);
 app.use("/api/products", ProductRouter);
-
-app.post("/show-data",  async (req, res) => {
-  const showData = await Users.findAll();
-  return res.status(200).json({success: true, data: showData});
-})
-
-app.delete("/del/:id", async(req, res) => {
-  const {id} = req.params;
-  await Users.destroy({where : {id}});
-  return res.status(200).json({success: true, message: "Data deleted successfully."});
-})
 
 app.listen(8080, () => {
     console.log("Server is listening...");
